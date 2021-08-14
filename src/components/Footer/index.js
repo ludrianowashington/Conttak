9097f65dd2842a272479672c0d42184db3f81d2e
Ludriano Washington <ludinho.washi@gmail.com>
2021-08-14 03:12:27 -0300

    Alterado

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,7 +11,7 @@ const Footer = () => {
 
   return(
     <Container>
-      <Text>Copyright 2021 Conttak, Inc. Todos os direitos reservados.</Text>
+      <Text>Copyright@2021 Conttak, Inc. Todos os direitos reservados.</Text>
       <Text2>Feito por Ludriano Washington</Text2>
     </Container>
   )
